669d1dbe1311f247160ab60eccbfb94765458844
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2022-05-13 16:07:30 -0700

    finished frontend code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { React, useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import {
+  Alert,
+  Button,
+  Container,
+  Navbar,
+  Nav,
+  Form,
+  Card,
+} from "react-bootstrap";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [userMessage, changeUserMessage] = useState(
+    "User message will appear here!"
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>NEAR Protocol</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'></Nav>
+            <Nav>
+              <Nav.Link eventKey={2} href='#memes'>
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row
+          style={{ marginTop: "10vh" }}
+          className='d-flex justify-content-center'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Your Message</Card.Title>
+              <Card.Subtitle className='mb-2 text-muted'>
+                Display Port for your message to the world! Or at lease Miami
+              </Card.Subtitle>
+              <Alert>{userMessage}</Alert>
+            </Card.Body>
+          </Card>
+        </Row>
+        <Row className='d-flex justify-content-center'>
+          <Form>
+            <Form.Group className='mb-3' controlId='formBasicPassword'>
+              <Form.Label>Enter Your Message Here</Form.Label>
+              <Form.Control placeholder='Enter Message' />
+            </Form.Group>
+
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
+          </Form>
+        </Row>
+      </Container>
+    </BrowserRouter>
   );
 }
 
